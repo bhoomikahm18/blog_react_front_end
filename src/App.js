2f0d741b2d752bd5180e0298bd06857f83feae08
Bhoomika <bhoomikahm18@gmail.com>
2023-11-20 10:56:21 +0530

    Adding links to pages

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import React from 'react';
 import './App.css';
 import Header from './components/Header';
+import { Route, Routes } from 'react-router-dom';
+import Auth from './components/Auth';
+import Blogs from './components/Blogs';
+import UserBlogs from './components/UserBlogs';
+import BlogDetail from './components/BlogDetail';
+import AddBlog from './components/AddBlog';
 
 function App() {
   return (
-    <Header />
+    <React.Fragment >
+      <header><Header /></header>
+      <main>
+        <Routes>
+          <Route path="/auth" element={<Auth />} />
+          <Route path="/blogs" element={<Blogs />} />
+          <Route path="/blogs/add" element={<AddBlog />} />
+          <Route path="/myBlogs" element={<UserBlogs />} />
+          <Route path="/myBlogs/:id" element={<BlogDetail />} />
+        </Routes>
+      </main>
+    </React.Fragment>
   );
 }
 

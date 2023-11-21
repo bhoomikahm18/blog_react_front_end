ceb1f5f353bc14f1adcf3b65dd5701feae340e08
Bhoomika <bhoomikahm18@gmail.com>
2023-11-21 14:51:15 +0530

    Creating navbar and setting up routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
 import React from 'react'
+import Header from './components/Header'
+import { Route, Routes } from 'react-router-dom'
+import Auth from './components/Auth.jsx'
+import Blogs from './components/Blogs.jsx'
+import UserBlog from './components/UserBlog.jsx'
+import BlogDetail from './components/BlogDetail.jsx'
+import AddBlog from './components/AddBlog.jsx'
 
 function App() {
   return (
-    <div>Hello World</div>
+    <>
+      <header>
+        <Header />
+      </header>
+      <main>
+        <Routes>
+          <Route path='/auth' element={<Auth />} />
+          <Route path='/blogs' element={<Blogs />} />
+          <Route path='/myBlogs' element={<UserBlog />} />
+          <Route path='/myBlogs/:id' element={<BlogDetail />} />
+          <Route path='/blogs/add' element={<AddBlog />} />
+        </Routes>
+      </main>
+    </>
   )
 }
 

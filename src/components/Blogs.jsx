717ab46d1b8a81c9dbde54f657db050fcfb8204a
Bhoomika <bhoomikahm18@gmail.com>
2023-11-23 09:13:37 +0530

    Displaying blogs in blog page

--- src/components/Blogs.jsx
+++ src/components/Blogs.jsx
@@ -1,8 +1,28 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import axios from 'axios';
+import { useState } from 'react';
+import Blog from './Blog.jsx';
 
 function Blogs() {
+  const [blogs, setBlogs] = useState();
+
+  async function sendRequest() {
+    const res = await axios
+      .get("http://localhost:5000/api/blog")
+      .catch(err => console.log(err))
+    const data = await res.data;
+    return data;
+  }
+  useEffect(() => {
+    sendRequest()
+      .then(data => setBlogs(data.blogs));
+  }, []);
+  console.log(blogs);
+
   return (
-    <div>Blogs</div>
+    <div>
+      <Blog />
+    </div>
   )
 }
 
